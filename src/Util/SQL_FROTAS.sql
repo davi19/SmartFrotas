28af669dcc276a04d7853fbba381cd237c619117
edevaldo <edevaldo@edevaldo>
2017-06-29 17:45:53 -0300

    SQL

--- src/Util/SQL_FROTAS.sql
+++ src/Util/SQL_FROTAS.sql
@@ -1,5 +1,4 @@
 CREATE TABLE Controle (
-id int NOT NULL AUTO_INCREMENT,
 codControle int PRIMARY KEY,
 codMotorista int,
 codVeiculo varchar(20),
@@ -10,7 +9,6 @@ dataEntrada date
 )
 
 CREATE TABLE Veiculo (
-id int NOT NULL AUTO_INCREMENT,
 placaVeiculo varchar(20) PRIMARY KEY,
 tipoVeiculo varchar(20),
 fabricante varchar(20),
@@ -18,14 +16,12 @@ modelo varchar(20),
 cor varchar(20),
 codVeiculo int,
 anoFabricacao int,
-numeroChassi int
+numeroChassi int,
 capacidadeTanque int,
 mediaConsumo float
-
 )
 
 CREATE TABLE Abastecimento (
-id int NOT NULL AUTO_INCREMENT,
 codAbastecimento int PRIMARY KEY,
 codVeiculo varchar(20),
 quantidadeAbastecida float,
@@ -35,7 +31,6 @@ dataAbastecimento date
 )
 
 CREATE TABLE Motorista (
-id int NOT NULL AUTO_INCREMENT,
 codMotorista int PRIMARY KEY,
 nomeMotorista varchar(40),
 categoriaCNH varchar(3),
@@ -44,7 +39,6 @@ dataDeEmissaoCNH date
 )
 
 CREATE TABLE UsuarioSistema (
-id int NOT NULL AUTO_INCREMENT,
 login varchar(40) PRIMARY KEY,
 senha varchar(10),
 nivelPermissao varchar(20)
